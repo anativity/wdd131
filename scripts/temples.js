ab4dc914d3ad1b347a384b01096bfaa0931b81ec
anativity <foxzep@gmail.com>
2026-03-17 21:47:39 -0400

    week2project

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
